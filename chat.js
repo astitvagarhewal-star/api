1979756ebcd2c6db2e7e1db7ced31d8d94bbb1b0
astitvagarhewal-star <astitva.garhewal@gmail.com>
2026-02-26 16:40:10 +0530

    Delete chat.js

--- chat.js
+++
@@ -1,9 +0,0 @@
-// This function runs every time someone visits /api/chat
-export default function handler(req, res) {
-  // res.status(200) means "Everything is OK"
-  // .json(...) is the data we are sending back
-  res.status(200).json({ 
-    reply: "Hello from Nexa",
-    methodUsed: req.method // This tells you if it was a GET or POST
-  });
-}
